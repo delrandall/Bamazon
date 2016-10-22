a8533e176906cc49eea9bc3122940e6429bf968c
delrandall <del_randall@hotmail.com>
2016-10-22 17:20:55 -0500

    Update Bamazon.sql

--- Bamazon.sql
+++ Bamazon.sql
@@ -14,11 +14,11 @@ INSERT INTO Bamazon.Products (ItemID, ProductName, DepartmentName, Price, StockQ
 VALUES 
 (1,"Clarity Defender® PLUS Automotive Windshield Treatment", "Auto", 1999, 10),
 (2, "Miller Beer in plastic bottles with clay nanoparticles", "Grocery", 999, 40),
-(3, "Body Armor with Fabric Coating",	"Armor",	500,	5),
-(4, "lithium iron phosphate battery",	"Home Tools",	7664,	10),
-(5, "Verigene system with DNA-coated gold nanoparticles",	"Pharmacy",	99999,	2),
-(6, "Palm-Tree Wax",	"Auto",	1999,	20),
-(7, "Nanosolar Panel Set (400 Watt Off Grid)",	"Home",	143572,	8),
-(8, "Pregnancy Test with Gold Nanoparticles",	"Pharmacy",	1298,	30),
-(9, "48 Tennis Balls with butyl rubber and vermiculite (bucket)",	"Sports",	4999,	30),
-(10, "Nosebleed Bandage with Aluminosilicate Nanoparticles (1 large)", "Pharmacy",	2400,	10)
+(3, "Body Armor with Fabric Coating", "Armor", 500,	5),
+(4, "lithium iron phosphate battery", "Home Tools",	7664, 10),
+(5, "Verigene system with DNA-coated gold nanoparticles", "Pharmacy", 99999, 2),
+(6, "Palm-Tree Wax", "Auto", 1999, 20),
+(7, "Nanosolar Panel Set (400 Watt Off Grid)", "Home", 143572, 8),
+(8, "Pregnancy Test with Gold Nanoparticles", "Pharmacy", 1298,	30),
+(9, "48 Tennis Balls with butyl rubber and vermiculite (bucket)", "Sports",	4999, 30),
+(10, "Nosebleed Bandage with Aluminosilicate Nanoparticles (1 large)", "Pharmacy", 2400, 10)
